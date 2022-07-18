f03dccd9aea39e6a241329adc771ca86500f0990
Kenneth Wilhelmsen <3333105+kenwi@users.noreply.github.com>
2022-07-18 23:53:29 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,17 @@ namespace _99_bottles_of_m0b
     {
         static void Main(string[] args)
         {
-            Enumerable.Range(0, 100).Reverse().ToList().ForEach(i => new System.Speech.Synthesis.SpeechSynthesizer().Speak(i > 0 ? $"{i} bottles of beer on the wall, {i} bottles of beer.\nTake one down, pass it around, {i-1} bottles of beer on the wall..." : "No more bottles of beer on the wall, no more bottles of beer."));
+            Enumerable
+                .Range(0, 100)
+                .Reverse()
+                .ToList()
+                .ForEach(i => new System
+                         .Speech
+                         .Synthesis
+                         .SpeechSynthesizer()
+                         .Speak(i > 0 
+                                ? $"{i} bottles of beer on the wall, {i} bottles of beer.\nTake one down, pass it around, {i-1} bottles of beer on the wall..."
+                                : "No more bottles of beer on the wall, no more bottles of beer."));
         }
     }
 }
