e94b24d8b3da01e359ea8569f890698962235fb3
Kenneth Wilhelmsen <3333105+kenwi@users.noreply.github.com>
2022-07-18 23:58:11 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,12 +10,10 @@ namespace _99_bottles_of_m0b
     {
         static void Main(string[] args)
         {
-            Enumerable
-                .Range(0, 100)
+            Enumerable.Range(0, 100)
                 .Reverse()
                 .ToList()
-                .ForEach(i => new System
-                         .Speech
+                .ForEach(i => new System.Speech
                          .Synthesis
                          .SpeechSynthesizer()
                          .Speak(i > 0 
